7384b960c447ac6d703d6c40be7e7bb2e5236e88
Thinnapop Srisomboon <thinapop.srisom@gmail.com>
2024-07-12 13:20:25 +0700

    edit Index.Java

--- Index.java
+++ Index.java
@@ -2,5 +2,6 @@ public class Index{
     public static void main(String args[]) {
         System.out.println("Namo so handsome");
         System.out.println("Namo so handsome");
+        System.out.println("Hi");
     }
 }
